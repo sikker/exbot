834ea798d235b46e3a653b6e1f4f26b88698411e
Per Sikker Hansen <per@sikker-hansen.dk>
2010-11-29 08:08:23 +0100

    Dummy functions for the war module

--- modules/war.mod.php
+++ modules/war.mod.php
@@ -5,6 +5,7 @@ if(isset($this->ex[4]))
 	$data = $this->module_data('war');
 	$message = FALSE;
 	$notice = FALSE;
+	// TODO: Some sort of purging/reminder routine, probably in a service
 	switch($this->ex[4])
 	{
 		case 'start':
@@ -19,11 +20,44 @@ if(isset($this->ex[4]))
 			else
 			{
 				$name = $data['names'][ rand(0,count($data['names'])-1) ];
-				$message = 'War started: ' . $name . ', woohoo!';
+				$delay = $this->ex[5];
+				$duration = $this->ex[6];
+				$message = 'Word war "' . $name . '" will start in ' . $delay . ' ' . ($delay!==1 ? 'minutes' : 'minute') . ' and will last for ' . $duration . ' ' . ($duration!==1 ? 'minutes' : 'minute');
+				// TODO: Save the war data in the data array
 			}
 			break;
-		case 'addname':
+		case 'join':
+			if( ! isset($this->ex[5]) || $this->ex[5] === 'last')
+			{
+				// Join the latest war, if any
+			}
+			else
+			{
+				// Join the desired war, if any
+			}
+			break;
+		case 'leave':
 			if( ! isset($this->ex[5]))
+			{
+				// NOTICE: You have to specify a war to leave
+			}
+			else
+			{
+				// Leave the desired war, if any
+			}
+			break;
+		case 'stop':
+			if( ! isset($this->ex[5]))
+			{
+				// NOTICE: you need to specify a war to stop
+			}
+			else
+			{
+				// Check if we're authed or if we started the war, and stop it if true
+			}
+			break;
+		case 'name':
+			if( ! isset($this->ex[5]) || $this->ex[5] === 'last')
 			{
 				$notice = 'No name given.';
 			}

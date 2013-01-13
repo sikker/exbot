5b5a5f2c4a922d5bf1bf6307b6f0fc053e7117f3
Per Sikker Hansen <persikkerhansen@gmail.com>
2013-01-13 19:27:43 +0100

    Sanitized data files and locations

--- data/about.dat.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$module_data = array();
-$service_data = array();
-
-// EOF
--- lib/exbotstorage/exbotstorage.class.php
+++ lib/exbotstorage/exbotstorage.class.php
@@ -2,6 +2,6 @@
 
 class ExbotStorage extends SimpleStorage {
 
-	protected $file = 'lib/exbotstorage/storage.json';
+	protected $file = 'data/storage.json';
 
 }

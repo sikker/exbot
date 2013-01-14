3a738d61c0aeee9cf8fe18b730cadd695a0d7fe3
Per Sikker Hansen <persikkerhansen@gmail.com>
2013-01-14 03:20:57 +0100

    Minor bugfix

--- exbot.php
+++ exbot.php
@@ -175,7 +175,7 @@ class ExBot extends IRCBot {
 // Start the bot
 if( ! isset($argv[1]) && ! isset($_GET['network'])) die('No network parameter provided, aborting' . PHP_EOL);
 
-if( ! file_exists(EXBOT_DIR) . 'config.php') {
+if( ! file_exists(EXBOT_DIR . 'config.php')) {
 	die('Rename config.php.example to config.php to get started.' . PHP_EOL);
 }
 
